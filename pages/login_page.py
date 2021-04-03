f16b1c06ac6b9bac70ea1dd645cbe9cb855667c8
Alexander Smirnov <alllecs@yandex.ru>
2021-04-03 11:51:54 +0300

    Added checking login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,22 +8,14 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        #try:
-        #    self.current_url(BasePage.url)
-        #except NoSuchElementException:
-        #    assert False
-        #assert True
+        # реализуйте проверку на корректный url адрес
+        login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        assert self.current_url(BasePage.url) == login_link, "Url is not corrected"
 
     def should_be_login_form(self):
-        try:
-            self.is_element_present(*LoginPageLocators.LOGIN_LINK)
-        except NoSuchElementException:
-            assert False, "No login link"
-        assert True
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def should_be_register_form(self):
-        try:
-            self.is_element_present(*LoginPageLocators.REG_LINK)
-        except NoSuchElementException:
-            assert False, "No register link"
-        assert True
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_LINK), "Register link is not presented"
