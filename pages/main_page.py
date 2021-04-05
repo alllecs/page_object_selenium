ff0abae61724d7c88705c63763d10a0348cc6141
Alexander Smirnov <alllecs@yandex.ru>
2021-04-05 12:58:28 +0300

    Added tests for can go to login page from base page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
